69d159bcb4b69d5337052d28e5e2d61b2658a0a4
Gustavo Lima <gustavoblima306@gmail.com>
2026-01-30 10:57:50 -0300

    feat: add core task management module

---
+++ task_tracker/core.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+DB_tasks = r"db/tasks.json"
+
+def load_db():
+    if not os.path.exists(DB_tasks):
+        return []
+    with open(DB_tasks, 'r') as file:
+        return json.load(file)
+
+def save_tasks(tasks):
+    with open(DB_tasks, 'w') as file:
+        json.dump(tasks, file, indent=2)
+
+def list_tasks():
+    return load_db()
+
+def add_task(title: str):
+    tasks = load_db()
+    new_task = {
+        'id': len(tasks) + 1,
+        'title': title,
+        'done': False
+    }
+    tasks.append(new_task)
+    save_tasks(tasks)
+    return new_task
