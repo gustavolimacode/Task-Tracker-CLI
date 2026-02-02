e9ec293ea5a2d2b4429a1a49b2268473cfe6e392
Gustavo Lima <gustavoblima306@gmail.com>
2026-02-01 21:12:10 -0300

    feat: add remove task command parser

--- task_tracker/cli.py
+++ task_tracker/cli.py
@@ -1,11 +1,12 @@
 import argparse
-from core import add_task, list_tasks
+from core import add_task, list_tasks, remove_task
 
 def main():
     parser = argparse.ArgumentParser(description="Task Tracker CLI")
 
-    parser.add_argument("command", choices=['add', 'list'], help="Task command to execute: add, list")
+    parser.add_argument("command", choices=['add', 'list', 'remove'], help="Task command to execute: add, list, remove")
     parser.add_argument("--title", help="Task title (Required for the 'add' command).")
+    parser.add_argument("--id", type=int, help="Task id (Required for the 'remove' command).")
 
     args = parser.parse_args()
 
@@ -24,6 +25,18 @@ def main():
         for task in tasks:
             status = 'x' if task.get('done') else " "
             print(f"[{status}] {task['id']} - {task['title']}")
+
+    elif args.command == 'remove':
+        if args.id is None:
+            print("Error: You need to specify the task id with '--id'.")
+            return
+        
+        removed = remove_task(args.id)
+
+        if removed:
+            print(f'Task {args.id} removed successfully.')
+        else:
+            print(f'Task with id {args.id} not found.')
 
 
 if __name__ == '__main__':
